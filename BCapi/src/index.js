d93494605cb871838b25f8dca709a4e6da7b2f09
Yatagarassu9 <rafaelyt47@gmail.com>
2026-04-23 15:46:30 -0300

    conecto la pagina a la api

--- BCapi/src/index.js
+++ BCapi/src/index.js
@@ -8,9 +8,19 @@ const app = express();
 
 try {
   app.use(express.json());
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });  
   app.use(bookRoutes);
-
+//app.use(authRoutes);
   await sequelize.sync();
+
+
+
+
 
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
